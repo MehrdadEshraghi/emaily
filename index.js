eb97e2964430f49871746db231a97f611a994895
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2020-12-06 15:29:17 +0330

    setup cookie-session with passport

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -13,6 +15,16 @@ mongoose
 	.catch((err) => console.log(err));
 
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [ keys.cookieKey ]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
