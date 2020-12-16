7e97ddfea5258d384e33af2fafb66954c7e10de5
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2020-12-16 15:22:48 +0330

    finish authentication flow in client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import { connect } from 'react-redux';
+import Landing from './Landing';
+import * as actions from '../actions';
 
-function App() {
+function Dashbord() {
 	return (
 		<div>
-			<h1>Hi there</h1>
+			<h2>Dashbord</h2>
+		</div>
+	);
+}
+function SurveyNew() {
+	return (
+		<div>
+			<h2>SurveyNew</h2>
 		</div>
 	);
 }
 
-export default App;
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route path="/" exact component={Landing} />
+						<Route path="/surveys" exact component={Dashbord} />
+						<Route path="/surveys/new" exact component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default connect(null, actions)(App);
