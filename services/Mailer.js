ac845f11d7f61117f4d7d8a8f8a8cb9bb91e35ca
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-02-16 19:55:27 +0330

    fix Mailer bug

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
 		super();
 
 		this.sgApi = sendgrid(keys.sendGridKey);
-		this.from_email = new helper.Email('micheal058200@gmail.com');
+		this.from_email = new helper.Email('emailyofficial0@gmail.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
 		this.recipients = this.formatAddresses(recipients);
